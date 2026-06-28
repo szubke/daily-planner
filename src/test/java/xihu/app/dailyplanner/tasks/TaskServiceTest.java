92ff91656e331ca2ba1f9709242ce95853235ab4
Krzysztof Szubertowicz <kszubertowicz@gmail.com>
2026-06-28 16:52:10 +0200

    add junit test for wrong id on findById method

--- src/test/java/xihu/app/dailyplanner/tasks/TaskServiceTest.java
+++ src/test/java/xihu/app/dailyplanner/tasks/TaskServiceTest.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import xihu.app.dailyplanner.tasks.exceptions.TaskNotFoundException;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -31,5 +35,11 @@ class TaskServiceTest {
         assertEquals("Kup mleko", result.getTitle());
         assertFalse(result.isCompleted());
         assertNotNull(result.getId());
+    }
+
+    @Test
+    void findTaskById_throwsWhenTaskNotFound() {
+        when(taskRepository.findById("zle-id")).thenReturn(Optional.empty());
+        assertThrows(TaskNotFoundException.class, () -> taskService.findTaskById("zle-id"));
     }
 }
